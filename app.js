eebd5a5a909e2dfd05315a236ca0dfb59a61772b
Irvin Alexanders <95768152+IrvinAlexanders@users.noreply.github.com>
2022-01-22 18:35:28 -0500

    Add the files

---
+++ app.js
@@ -0,0 +1,428 @@
+function crea() {
+    var info = document.getElementById("info");
+    var scuole = document.getElementById("scuole");
+    var lavori = document.getElementById("lavori");
+    var hard = document.getElementById("hard");
+    var soft = document.getElementById("soft");
+    var goal = document.getElementById("goal");
+    var lingue = document.getElementById("lingue");
+    var fama = document.getElementById("fama");
+    var fine = document.getElementById("fine");
+    
+    var opz = document.getElementById("opz");
+    
+    var info2 = document.getElementById("info2");
+    var scuole2 = document.getElementById("scuole2");
+    var lavori2 = document.getElementById("lavori2");
+    var hard2 = document.getElementById("hard2");
+    var soft2 = document.getElementById("soft2");
+    var goal2 = document.getElementById("goal2");
+    var lingue2 = document.getElementById("lingue2");
+    var fama2 = document.getElementById("fama2");
+    var fine2 = document.getElementById("fine2");
+    
+    var h3 = document.getElementById("h3");
+    var h4 = document.getElementById("h4");
+    var finale = document.getElementById("finale");
+    
+    info.style.display = "block";
+    scuole.style.display = "block";
+    lavori.style.display = "block";
+    hard.style.display = "block";
+    soft.style.display = "block";
+    goal.style.display = "block";
+    lingue.style.display = "block";
+    fama.style.display = "block";
+    fine.style.display = "block";
+    opz.style.display = "none";
+    h3.style.display = "none";
+    h4.style.display = "block";
+    finale.style.display = "none";
+//Pressing "CV Builder App"   
+$(function(){
+    $("#h3").on("click",function(){
+    opz.style.display = "none";
+    h3.style.display = "none";
+    h4.style.display = "block";
+    finale.style.display = "none";
+    
+    info.style.display = "block";
+    scuole.style.display = "block";
+    lavori.style.display = "block";
+    hard.style.display = "block";
+    soft.style.display = "block";
+    goal.style.display = "block";
+    lingue.style.display = "block";
+    fama.style.display = "block";
+    fine.style.display = "block";
+    
+    info2.style.display = "none";
+    scuole2.style.display = "none";
+    lavori2.style.display = "none";
+    hard2.style.display = "none";
+    soft2.style.display = "none";
+    goal2.style.display = "none";
+    lingue2.style.display = "none";
+    fama2.style.display = "none";
+    fine2.style.display = "none";
+    })
+})    
+$(function(){
+    $("#info").on("click",function(){
+    info2.style.display = "block";
+    h3.style.display = "block";
+    h4.style.display = "none";
+    finale.style.display = "none";
+    
+    info.style.display = "none";
+    scuole.style.display = "none";
+    lavori.style.display = "none";
+    hard.style.display = "none";
+    soft.style.display = "none";
+    goal.style.display = "none";
+    lingue.style.display = "none";
+    fama.style.display = "none";
+    opz.style.display = "none";
+    fine.style.display = "none";
+    
+    scuole2.style.display = "none";
+    lavori2.style.display = "none";
+    hard2.style.display = "none";
+    soft2.style.display = "none";
+    goal2.style.display = "none";
+    lingue2.style.display = "none";
+    fama2.style.display = "none";
+    fine2.style.display = "none";
+    })
+}) 
+$(function(){
+    $("#scuole").on("click",function(){
+    scuole2.style.display = "block";
+    h3.style.display = "block";
+    h4.style.display = "none";
+    finale.style.display = "none";
+    
+    info.style.display = "none";
+    scuole.style.display = "none";
+    lavori.style.display = "none";
+    hard.style.display = "none";
+    soft.style.display = "none";
+    goal.style.display = "none";
+    lingue.style.display = "none";
+    fama.style.display = "none";
+    opz.style.display = "none";
+    fine.style.display = "none";
+    
+    info2.style.display = "none";
+    lavori2.style.display = "none";
+    hard2.style.display = "none";
+    soft2.style.display = "none";
+    goal2.style.display = "none";
+    lingue2.style.display = "none";
+    fama2.style.display = "none";
+    fine2.style.display = "none";
+    })
+})
+$(function(){
+    $("#lavori").on("click",function(){
+    lavori2.style.display = "block";
+    h3.style.display = "block";
+    h4.style.display = "none";
+    finale.style.display = "none";
+    
+    info.style.display = "none";
+    scuole.style.display = "none";
+    lavori.style.display = "none";
+    hard.style.display = "none";
+    soft.style.display = "none";
+    goal.style.display = "none";
+    lingue.style.display = "none";
+    fama.style.display = "none";
+    opz.style.display = "none";
+    fine.style.display = "none";
+    
+    info2.style.display = "none";
+    scuole2.style.display = "none";
+    hard2.style.display = "none";
+    soft2.style.display = "none";
+    goal2.style.display = "none";
+    lingue2.style.display = "none";
+    fama2.style.display = "none";
+    fine2.style.display = "none";
+    })
+})
+$(function(){
+    $("#hard").on("click",function(){
+    hard2.style.display = "block";
+    h3.style.display = "block";
+    h4.style.display = "none";
+    finale.style.display = "none";
+    
+    info.style.display = "none";
+    scuole.style.display = "none";
+    lavori.style.display = "none";
+    hard.style.display = "none";
+    soft.style.display = "none";
+    goal.style.display = "none";
+    lingue.style.display = "none";
+    fama.style.display = "none";
+    opz.style.display = "none";
+    fine.style.display = "none";
+    
+    scuole2.style.display = "none";
+    lavori2.style.display = "none";
+    info2.style.display = "none";
+    soft2.style.display = "none";
+    goal2.style.display = "none";
+    lingue2.style.display = "none";
+    fama2.style.display = "none";
+    fine2.style.display = "none";
+    })
+})
+$(function(){
+    $("#soft").on("click",function(){
+    soft2.style.display = "block";
+    h3.style.display = "block";
+    h4.style.display = "none";
+    finale.style.display = "none";
+    
+    info.style.display = "none";
+    scuole.style.display = "none";
+    lavori.style.display = "none";
+    hard.style.display = "none";
+    soft.style.display = "none";
+    goal.style.display = "none";
+    lingue.style.display = "none";
+    fama.style.display = "none";
+    opz.style.display = "none";
+    fine.style.display = "none";
+    
+    scuole2.style.display = "none";
+    lavori2.style.display = "none";
+    hard2.style.display = "none";
+    info2.style.display = "none";
+    goal2.style.display = "none";
+    lingue2.style.display = "none";
+    fama2.style.display = "none";
+    fine2.style.display = "none";
+    })
+})
+$(function(){
+    $("#goal").on("click",function(){
+    goal2.style.display = "block";
+    h3.style.display = "block";
+    h4.style.display = "none";
+    finale.style.display = "none";
+    
+    info.style.display = "none";
+    scuole.style.display = "none";
+    lavori.style.display = "none";
+    hard.style.display = "none";
+    soft.style.display = "none";
+    goal.style.display = "none";
+    lingue.style.display = "none";
+    fama.style.display = "none";
+    opz.style.display = "none";
+    fine.style.display = "none";
+    
+    scuole2.style.display = "none";
+    lavori2.style.display = "none";
+    hard2.style.display = "none";
+    soft2.style.display = "none";
+    info2.style.display = "none";
+    lingue2.style.display = "none";
+    fama2.style.display = "none";
+    fine2.style.display = "none";
+    })
+})
+$(function(){
+    $("#lingue").on("click",function(){
+    lingue2.style.display = "block";
+    h3.style.display = "block";
+    h4.style.display = "none";
+    finale.style.display = "none";
+    
+    info.style.display = "none";
+    scuole.style.display = "none";
+    lavori.style.display = "none";
+    hard.style.display = "none";
+    soft.style.display = "none";
+    goal.style.display = "none";
+    lingue.style.display = "none";
+    fama.style.display = "none";
+    opz.style.display = "none";
+    fine.style.display = "none";
+    
+    scuole2.style.display = "none";
+    lavori2.style.display = "none";
+    hard2.style.display = "none";
+    soft2.style.display = "none";
+    goal2.style.display = "none";
+    info2.style.display = "none";
+    fama2.style.display = "none";
+    fine2.style.display = "none";
+    })
+})
+$(function(){
+    $("#fama").on("click",function(){
+    fama2.style.display = "block";
+    h3.style.display = "block";
+    h4.style.display = "none";
+    finale.style.display = "none";
+    
+    info.style.display = "none";
+    scuole.style.display = "none";
+    lavori.style.display = "none";
+    hard.style.display = "none";
+    soft.style.display = "none";
+    goal.style.display = "none";
+    lingue.style.display = "none";
+    fama.style.display = "none";
+    opz.style.display = "none";
+    fine.style.display = "none";
+    
+    scuole2.style.display = "none";
+    lavori2.style.display = "none";
+    hard2.style.display = "none";
+    soft2.style.display = "none";
+    goal2.style.display = "none";
+    lingue2.style.display = "none";
+    info2.style.display = "none";
+    fine2.style.display = "none";
+    })
+})
+$(function(){
+    $("#fine").on("click",function(){
+    fine2.style.display = "block";
+    h3.style.display = "block";
+    h4.style.display = "none";
+    finale.style.display = "none";
+    
+    info.style.display = "none";
+    scuole.style.display = "none";
+    lavori.style.display = "none";
+    hard.style.display = "none";
+    soft.style.display = "none";
+    goal.style.display = "none";
+    lingue.style.display = "none";
+    fama.style.display = "none";
+    opz.style.display = "none";
+    fine.style.display = "none";
+    
+    scuole2.style.display = "none";
+    lavori2.style.display = "none";
+    hard2.style.display = "none";
+    soft2.style.display = "none";
+    goal2.style.display = "none";
+    lingue2.style.display = "none";
+    fama2.style.display = "none";
+    info2.style.display = "none";
+    })
+})
+
+}
+
+//Inspired from Pavel Durov
+var count=0;
+function lavori(){
+    $("#lavori2").append("<div id=\'lavoro2"+count+"\'><input type='text' placeholder='New Job' id=\'mansione2"+count+"\'><br /><input type='text' placeholder='New Job Location' id=\'ufficio2"+count+"\'><br /><button onclick='togli("+count+")'>X</button></div><br />");
+    count+=1;
+    return count;
+}
+function togli(i)
+{
+    $("#lavoro2"+i).remove();
+}
+function scuole(){
+    $("#scuole2").append("<div id=\'scuole2"+count+"\'><input type='text' placeholder='Title' id=\'titolo2"+count+"\'><br /><input type=text' placeholder='School' id=\'studio2"+count+"\'><br /><input type='number' placeholder='Mark' id=\'voto2"+count+"\'><br /><button onclick='togliamo("+count+")'>X</button></div><br />");
+    count+=1;
+    return count;
+}
+function togliamo(i)
+{
+    $("#scuole2"+i).remove();
+}
+function hard(){
+    $("#hard2").append("<div id=\'hard2"+count+"\'><input type='text' placeholder='Hard Skill' id=\'hardabil2"+count+"\'><br /><button onclick='togliere("+count+")'>X</button></div><br />");
+    count+=1;
+    return count;
+}
+function togliere(i)
+{
+    $("#hard2"+i).remove();
+}
+function soft(){
+    $("#soft2").append("<div id=\'soft2"+count+"\'><input type='text' placeholder='Soft Skill' id=\'softabil2"+count+"\'><br /><button onclick='toglierei("+count+")'>X</button></div><br />");
+    count+=1;
+    return count;
+}
+function toglierei(i)
+{
+    $("#soft2"+i).remove();
+}
+function lingue(){
+    $("#lingue2").append("<div id=\'lingue2"+count+"\'><input type='text' placeholder='New Language' id=\'linguaggio2"+count+"\'><br /><button onclick='toglierebbe("+count+")'>X</button></div><br />");
+    count+=1;
+    return count;
+}
+function toglierebbe(i)
+{
+    $("#lingue2"+i).remove();
+}
+function fama(){
+    $("#fama2").append("<div id=\'fama2"+count+"\'><input type='text' placeholder='New Reference' id=\'contatti2"+count+"\'><br /><button onclick='togliersi("+count+")'>X</button></div><br />");
+    count+=1;
+    return count;
+}
+function togliersi(i)
+{
+    $("#fama2"+i).remove();
+}
+$(function(){
+    $("#modello1").on("click",function(){
+        document.getElementById("finale").style.display = "block";
+        var nome =  document.getElementById("nome").value;
+        var cognome = document.getElementById("cognome").value;
+        var email = document.getElementById("email").value;
+        var numero = document.getElementById("numero").value;
+        var posto = document.getElementById("posto").value;
+        var mansione = document.getElementById("mansione").value;
+        var ufficio = document.getElementById("ufficio").value;
+        var titolo = document.getElementById("titolo").value;
+        var studio = document.getElementById("studio").value;
+        var voto = document.getElementById("voto").value;
+        var hard = document.getElementById("hardabil").value;
+        var soft = document.getElementById("softabil").value;
+        var lingue = document.getElementById("lingue").value;
+        var obbiettivo = document.getElementById("obbiettivo").value;
+        var contatti = document.getElementById("contatti").value;
+        document.getElementById("result1").innerHTML="<center><h3>Model 1</h3><hr width='87%'><br /><br />"+nome+" "+cognome+"<br />"+email+"<br />"+numero+"<br />"+posto+"<br /><h4><q>"+obbiettivo+"</q></h4></center>";
+        document.getElementById("result2").innerHTML="<center><h4>Job Experiences</h4><hr width='87%'></center><b>"+mansione+"</b><br />"+ufficio+"<center><h4>Job References</h4><hr width='87%'></center>"+contatti;
+        document.getElementById("result3").innerHTML="<center><h4>Education</h4><hr width='87%'></center><b>"+titolo+"</b><br />"+studio+"<br />"+voto+"/100";
+        document.getElementById("result4").innerHTML="<center><h4>Hard Skills</h4><hr width='87%'></center><b>"+hard+"</b><br /><center><h4>Soft Skills</h4><hr width='87%'></center><b>"+soft+"</b>";
+    })
+})
+$(function(){
+    $("#modello2").on("click",function(){
+        document.getElementById("finale").style.display = "block";
+        var nome =  document.getElementById("nome").value;
+        var cognome = document.getElementById("cognome").value;
+        var email = document.getElementById("email").value;
+        var numero = document.getElementById("numero").value;
+        var posto = document.getElementById("posto").value;
+        var mansione = document.getElementById("mansione").value;
+        var ufficio = document.getElementById("ufficio").value;
+        var titolo = document.getElementById("titolo").value;
+        var studio = document.getElementById("studio").value;
+        var voto = document.getElementById("voto").value;
+        var hard = document.getElementById("hardabil").value;
+        var soft = document.getElementById("softabil").value;
+        var lingue = document.getElementById("lingue").value;
+        var obbiettivo = document.getElementById("obbiettivo").value;
+        var contatti = document.getElementById("contatti").value;
+        document.getElementById("result1").innerHTML="<center><h3>Model 2</h3><hr width='87%'></center><br /><br /><p>Hi, I am <b>"+nome+" "+cognome+".</b> My email is <b>"+email+"</b> and my phone number is <b>"+numero+".</b><br />I live in "+posto+"<br />My life motto is <b><q>"+obbiettivo+"</q></b></p>";
+        document.getElementById("result2").innerHTML="<h4>Job Experiences</h4><hr width='87%'></center><p>I had the opportunity to work as a <b>"+mansione+"</b> while working for <b>"+ufficio+"</b>, but for any contact you can tell to <b>"+contatti+"</b></p>";
+        document.getElementById("result3").innerHTML="<h4>Education</h4><hr width='87%'><p>I have a <b>"+titolo+"</b> at <br />"+studio+",</b> with a final score of <b>"+voto+"/100.</b></p>";
+        document.getElementById("result4").innerHTML="<h4>Skills</h4><hr width='87%'><p>I think my greatest skills are <b>"+hard+"</b> and <b>"+soft+"</b></p>";
+    })
+})
